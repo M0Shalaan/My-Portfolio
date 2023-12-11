99b3e038fbfc552996cc38b9e87e5ad3bdf2d823
Mohamed Shalaan <z8man@gmail.com>
2023-12-11 15:54:14 +0200

    added logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    document.title = "My-Portfolio";
+  },[]);
   return (
     <BrowserRouter>
       <Routes>
